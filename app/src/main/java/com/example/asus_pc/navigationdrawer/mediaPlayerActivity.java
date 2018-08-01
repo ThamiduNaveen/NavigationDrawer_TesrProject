333fb5e2d75fb402fafde17a6d7b6bb49c6cf7b9
thamiduNaveen <thamidunaveen@gmail.com>
2018-08-01 14:30:26 +0530

    this is 4 commit

--- app/src/main/java/com/example/asus_pc/navigationdrawer/mediaPlayerActivity.java
+++ app/src/main/java/com/example/asus_pc/navigationdrawer/mediaPlayerActivity.java
@@ -1,7 +1,10 @@
 package com.example.asus_pc.navigationdrawer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class mediaPlayerActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class mediaPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
+
+        Button playBT =(Button)findViewById(R.id.button_mediaPlayer_play);
+
+        final MediaPlayer mplay = MediaPlayer.create(mediaPlayerActivity.this,R.raw.sample_audio);
+
+        playBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mplay.start();
+            }
+        });
     }
 }
